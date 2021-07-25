dbbf23bc4e5617ccc62e4b1fbb70a1220c281cb6
sejesila <silaseje5@gmail.com>
2021-07-25 10:51:07 +0300

    fixed all routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Transaction;
 use App\Http\Controllers\TransactionController;
 
 /*
@@ -16,12 +17,12 @@ use App\Http\Controllers\TransactionController;
 
 Route::redirect('/', '/transactions');
 
-Route::get('transactions/{transactions}/export',
+Route::get('transactions/{transaction}/export',
     [TransactionController::class, 'export'])
     ->name('transactions.export');
 
 Route::resource('transactions', TransactionController::class);
 
-Route::get('transactions/{transaction}/duplicate',
+Route::get('transactions/{transaction:uuid}/duplicate',
     [TransactionController::class, 'duplicate'])
     ->name('transactions.duplicate');
