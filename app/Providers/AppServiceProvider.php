89ed1081c27d93b552574588a2dfa2dd4697058b
Plamen Markov <plamen@lynxlake.org>
2021-07-25 10:03:30 +0300

    All tasks done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Paginator::useBootstrap();
     }
 }
