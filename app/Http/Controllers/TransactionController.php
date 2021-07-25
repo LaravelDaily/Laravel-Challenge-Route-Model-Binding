0a98322c725611d5ebb71ad209447e1a39fa17fc
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-07-25 09:11:16 +0300

    finish task

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
+
 class TransactionController extends Controller
 {
-    public function index()
-    {
-        $transactions = \App\Models\Transaction::with('user')->get();
+  public function index()
+  {
+    $transactions = \App\Models\Transaction::with('user')->get();
 
-        return view('transactions.index', compact('transactions'));
-    }
+    return view('transactions.index', compact('transactions'));
+  }
 
-    public function show(Transaction $transaction)
-    {
-        return view('transactions.show', compact('transaction'));
-    }
+  public function show(Transaction $transaction)
+  {
 
-    public function export(Transaction $transaction)
-    {
-        return view('transactions.export', compact('transaction'));
-    }
+    return view('transactions.show', compact('transaction'));
+  }
 
-    public function duplicate(Transaction $transaction)
-    {
-        return view('transactions.duplicate', compact('transaction'));
-    }
+  public function export(Transaction $transaction)
+  {
+    return view('transactions.export', compact('transaction'));
+  }
+
+  public function duplicate(Transaction $transaction)
+  {
+    return view('transactions.duplicate', compact('transaction'));
+  }
 }
