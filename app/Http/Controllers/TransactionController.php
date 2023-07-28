3a69a73517f0bfe971c8615e6ef6d87c2acc810b
TaukirAhmed <33090525+taukirXi@users.noreply.github.com>
2023-07-29 02:44:32 +0600

    task 1 complete

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
+
 class TransactionController extends Controller
 {
     public function index()
@@ -11,8 +13,9 @@ class TransactionController extends Controller
         return view('transactions.index', compact('transactions'));
     }
 
-    public function show(Transaction $transaction)
+    public function show($id)
     {
+        $transaction = Transaction::findOrFail($id);
         return view('transactions.show', compact('transaction'));
     }
 
